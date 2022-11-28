8db3de489af805c3dec57dc01149fec63e7efe7a
DinhDieu <dinhthianhdieu@gmail.com>
2022-11-28 22:27:35 +0700

    check git

--- src/test/java/demo/api/LoginApiTest.java
+++ src/test/java/demo/api/LoginApiTest.java
@@ -13,6 +13,7 @@ public class LoginApiTest {
   @Test
   public  void check_logi(){
     Map<String, Object> request = new HashMap<>();
+//    check git
     LoginDTO loginDTO = LoginDTO.builder().username("105C056630").password("12345678").build();
     Response response = RequestAPI.post("https://apipub.tcbs.com.vn/authen/v1/login", request, loginDTO);
     Assert.assertEquals("Kiem tra tra ve ma loi", 400, response.getStatusCode() );
